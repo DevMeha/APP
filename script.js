734af4be2ab51f24201a388a33494ae596f0681b
Meha <mixiadami@gmail.com>
2025-06-26 20:09:43 +0200

    ADD SWITCH PLAYER AND BACGGRAOUD COLOR IF PLAYER ROLL 1

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 'use strict';
-
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
-const dice = document.querySelector('.dice');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnNEW = document.querySelector('.btn--hold');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
-dice.classList.add('hidden');
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+// ROLLING DICE FUNCTIONALITY
+
+btnRoll.addEventListener('click', function () {
+  // GENERATTING A RANDOM DICE ROLL
+  // DISPLAY DICE
+  // CHECK FOR ROLLED IF TRUE SWITCH TO NEXT PLAYER
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  if (dice !== 1) {
+    // ADD CURRENT SCORE BO JEST INNE OD 1
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // SWITCH NEXT PLAYER BO WYSLOWALA SIE 1
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+});
